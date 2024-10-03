2ee64bbd25fd0d81a596a6a060549b674d063c6d
Jake <jacobsolsky@gmail.com>
2024-10-03 16:12:07 +1000

    updated CORS config

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigin = [
     'https://greenhouse-be-m9d5.onrender.com',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://greenhouse-fe.onrender.com'
 ]
 
 module.exports = allowedOrigin
